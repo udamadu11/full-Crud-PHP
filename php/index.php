cdea1cdeecfbfa813b564c526a4a4b4ca26a1d7f
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-28 16:27:28 +0530

    0.5

--- php/index.php
+++ php/index.php
@@ -119,20 +119,21 @@ if (isset($_POST['submit2'])) {
 
 if (isset($_POST['submit1'])) {
 	$edit = $_POST['edit'];
-	$sql4 = "SELECT * FROM stu";
+	$sql4 = "SELECT * FROM stu WHERE id= $edit";
 	$result4 = mysqli_query($con,$sql4);
-	while ($row1 = mysqli_fetch_array($result4)) {
+	while ($row = mysqli_fetch_array($result4)) {
 		echo "
-			<form name=\"StudentForm\" method=\"post\">
+		<div class=\"container\" style=\"width: 50%;border: 2px solid green;padding: 30px;\">
+			<form method=\"post\">
 			<div class=\"form-group\">
 				<div class=\"row\">
 				<div class=\"col-md-6\">
 				<label>Student Name</label>
-				<input type=\"text\" name=\"stuName\" class=\"form-control\">
+				<input type=\"text\" name=\"stuName\" class=\"form-control\" value=\"{$row['stu_name']}\">
 				</div>
 				<div class=\"col-md-6\">
 					<label>Gender</label>
-					<select class=\"form-control\" name=\"gender\">
+					<select class=\"form-control\" name=\"gender\" value=\"{$row['gender']}\">
 						<option value=\"M\">Male</option>
 						<option value=\"F\">Female</option>
 					</select>
@@ -143,25 +144,46 @@ if (isset($_POST['submit1'])) {
 				<div class=\"row\">
 					<div class=\"col-md-4\">
 					<label>Econ</label>
-					<input type=\"number\" name=\"secon\" class=\"form-control\">
+					<input type=\"number\" name=\"secon\" class=\"form-control\" value=\"{$row['econ']}\">
 				</div>
 				<div class=\"col-md-4\">
 					<label>Accounting</label>
-					<input type=\"number\" name=\"saccount\" class=\"form-control\">
+					<input type=\"number\" name=\"saccount\" class=\"form-control\" value=\"{$row['accounting']}\">
 				</div>
 				<div class=\"col-md-4\">
 					<label>Bussiness</label>
-					<input type=\"number\" name=\"sBussiness\" class=\"form-control\">
+					<input type=\"number\" name=\"sBussiness\" class=\"form-control\" value=\"{$row['bussiness']}\">
 				</div>
 				</div>
 				
 			</div>
 			<div class=\"form-group\">
-				<input type=\"submit\" name=\"submit\" class=\"btn btn-success\" value=\"Add\" style=\"width: 100%;height: 40px;margin-top: 20px;\">
+				<input type=\"hidden\" value=" .$row['id']. " name=\"EditID\">
+				<input type=\"submit\" name=\"submit4\" class=\"btn btn-success\" value=\"Update\" style=\"width: 100%;height: 40px;margin-top: 20px;\">
 			</div>
 			</fotm>
+			</div>
 		";
 	}
+}
+
+if (isset($_POST['submit4'])) {
+	$uid = $_POST['EditID'];
+        $newFname = $_POST['EditFName'];
+        $newLname = $_POST['EditLName'];
+        $newUname = $_POST['EditUName'];
+        $newEmail = $_POST['EditEmail'];
+        $newTelephone = $_POST['EditTelephone'];
+        $newNic = $_POST['EditNic'];
+        $newPassword = $_POST['EditPassword'];
+        $newType = $_POST['EditType'];
+
+        $EditQuery= "UPDATE employee SET f_name ='$newFname',l_name = '$newLname',u_name = '$newUname',email ='$newEmail',telephone ='$newTelephone',nic ='$newNic',password ='$newPassword',type ='$newType' WHERE id = '$uid' ";
+        $sqlQuery = mysqli_query($con,$EditQuery);
+        if ($sqlQuery) {
+            echo "<script>alert('Successfuly Upadated...')</script>";
+            echo "<script>window.open('editUser.php','_self')</script>";
+        }
 }
 
 ?>
